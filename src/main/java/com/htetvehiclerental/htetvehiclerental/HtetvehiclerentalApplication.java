fcf1be97d1a18c8e986cfef9703630337698626c
Htet Myet Aung Naing <htetmyet.2001@gmail.com>
2025-08-11 16:05:42 -0600

    added cors config file

--- src/main/java/com/htetvehiclerental/htetvehiclerental/HtetvehiclerentalApplication.java
+++ src/main/java/com/htetvehiclerental/htetvehiclerental/HtetvehiclerentalApplication.java
@@ -1,5 +1,6 @@
 package com.htetvehiclerental.htetvehiclerental;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class HtetvehiclerentalApplication {
 
     public static void main(String[] args)
     {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("spring.mail.username", dotenv.get("SPRING_MAIL_USERNAME"));
+        System.setProperty("spring.mail.password", dotenv.get("SPRING_MAIL_PASSWORD"));
         SpringApplication.run(HtetvehiclerentalApplication.class, args);
     }
 
