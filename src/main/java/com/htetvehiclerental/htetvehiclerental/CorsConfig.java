a9bef6b888c92efecfd98d69e4aa7816af1b4e83
Htet Myet Aung Naing <htetmyet.2001@gmail.com>
2025-08-11 16:11:49 -0600

    changed cors origin for testing

--- src/main/java/com/htetvehiclerental/htetvehiclerental/CorsConfig.java
+++ src/main/java/com/htetvehiclerental/htetvehiclerental/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:4200") // add your frontend ports here
+                        .allowedOrigins("*") // add your frontend ports here
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
