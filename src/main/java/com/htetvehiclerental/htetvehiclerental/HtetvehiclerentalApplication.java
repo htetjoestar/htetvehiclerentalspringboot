0b94a9fa87307cb4d7cb1c2de01ca961633e7704
Htet Myet Aung Naing <htetmyet.2001@gmail.com>
2025-08-11 16:36:01 -0600

    Changed need for .env so app can run on azure

--- src/main/java/com/htetvehiclerental/htetvehiclerental/HtetvehiclerentalApplication.java
+++ src/main/java/com/htetvehiclerental/htetvehiclerental/HtetvehiclerentalApplication.java
@@ -7,14 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HtetvehiclerentalApplication {
 
-    public static void main(String[] args)
-    {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-        System.setProperty("spring.mail.username", dotenv.get("SPRING_MAIL_USERNAME"));
-        System.setProperty("spring.mail.password", dotenv.get("SPRING_MAIL_PASSWORD"));
+
+    public static void main(String[] args) {
+        System.setProperty("spring.datasource.url", System.getenv("SPRING_DATASOURCE_URL"));
+        System.setProperty("spring.datasource.username", System.getenv("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("spring.datasource.password", System.getenv("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("spring.mail.username", System.getenv("SPRING_MAIL_USERNAME"));
+        System.setProperty("spring.mail.password", System.getenv("SPRING_MAIL_PASSWORD"));
+
         SpringApplication.run(HtetvehiclerentalApplication.class, args);
     }
 
